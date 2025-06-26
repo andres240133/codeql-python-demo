b9956ed9fb96d0cd75ee52b8fb530c1981d6129c
andres240133 <andresmauricio2401@gmail.com>
2025-06-25 21:14:00 -0600

    Update vulnerable.py

--- vulnerable.py
+++ vulnerable.py
@@ -1,10 +1,10 @@
 import os
 
 def ejecutar_comando(usuario):
-    print(f"Ejecutando comando para el usuario: {usuario}")
-    comando = f"ping {usuario}"  # <- Riesgo de inyección de comandos
+    comando = f"ping {usuario}"  # <- Riesgo: el input se inserta directamente en un comando del sistema
     os.system(comando)
 
-nombre_usuario = input("Ingrese una dirección o IP para hacer ping: ")
-ejecutar_comando(nombre_usuario)
+nombre = input("Ingrese dirección IP o dominio: ")
+ejecutar_comando(nombre)
+
 
